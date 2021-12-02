93a6bb52610cc7bf857837475f7c4d594b2f0668
Devvrt <76960535+Devvrt@users.noreply.github.com>
2021-12-02 18:28:09 -0500

    et

--- preditction.py
+++ preditction.py
@@ -5,19 +5,21 @@ import numpy as np
 import scipy.stats as sst
 import matplotlib.pyplot as plt
 
-
+# 
 # prec = pd.read_csv("C:\\Users\\bhard\\OneDrive\\Desktop\\SYSC5405_Project\\xgb,et,svm-10_pr@50rc.csv", names=["Precision"])
 
-prec = pd.read_csv("C:\\Users\\bhard\\OneDrive\\Desktop\\SYSC5405_Project\\dt_pr@50rc.csv", names=["Precision"])
+# prec = pd.read_csv("C:\\Users\\bhard\\OneDrive\\Desktop\\SYSC5405_Project\\dt_pr@50rc.csv", names=["Precision"])
 
+# prec = pd.read_csv("C:\\Users\\bhard\\OneDrive\\Desktop\\SYSC5405_Project\\dt_opt_pr@50rc.csv", names=["Precision"])
 
+# prec = pd.read_csv("C:\\Users\\bhard\\OneDrive\\Desktop\\SYSC5405_Project\\.csv", names=["Precision"])
 
 precision_values = prec["Precision"].tolist()
 
 print(precision_values)
 # print("For XGB- ET - SVM")
 
-print("For DT Vanilla")
+print("For ET")
 
 print("Mean: ",np.mean(precision_values))
 print("STD: ",np.std(precision_values))
