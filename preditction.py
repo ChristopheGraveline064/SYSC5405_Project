9d4ad0792725ae4e1464c3292cddddfc5d63d0c6
Devvrt <76960535+Devvrt@users.noreply.github.com>
2021-12-02 17:54:01 -0500

    mean_std_script

---
+++ preditction.py
@@ -0,0 +1,22 @@
+#%%
+
+import pandas as pd
+import numpy as np
+import scipy.stats as sst
+import matplotlib.pyplot as plt
+
+
+prec = pd.read_csv("C:\\Users\\bhard\\OneDrive\\Desktop\\SYSC5405_Project\\xgb,et,svm-10_pr@50rc.csv", names=["Precision"])
+
+
+
+
+precision_values = prec["Precision"].tolist()
+
+
+print("For XGB- ET - SVM")
+print("Mean: ",np.mean(precision_values))
+print("STD: ",np.std(precision_values))
+# print("VAR: ",np.var(precision_values))
+
+
